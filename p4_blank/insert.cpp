ff07e0195f51aa2fc48e3be734ce32ea91a2f34f
Heri Zhao <zhaoheri@gmail.com>
2014-11-19 20:06:52 -0500

    insert parrially done, without error checking

--- p4_blank/insert.cpp
+++ p4_blank/insert.cpp
@@ -1,6 +1,9 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
+#include <string.h>
+#include <iostream>
+#include <stdlib.h>
 
 /*
  * Inserts a record into the specified relation
@@ -16,30 +19,51 @@ Status Updates::Insert(const string& relation,      // Name of the relation
 {
     /* Your solution goes here */
 
+    //For error check: go to piazza @1009
+    
     //Get the attrLen for each element in attrList[]
-    //And reorder all the elements into the new array: attrInfo insertAttrList[]
+    //And re-order all the elements into the new array: attrInfo insertAttrList[]
+    Record record;
     int attrNum;
-    int totalSizeinBytes = 0;
+    record.length = 0;
     AttrDesc *attrs = new AttrDesc[attrCnt];
     attrCat->getRelInfo(relation, attrNum, attrs);
     attrInfo *insertAttrList = new attrInfo[attrCnt];
     for(int i = 0; i < attrCnt; i++)
-    {
+    { 
     	char attrName[MAXNAME];
     	strcpy(attrName, attrs[i].attrName);
     	for(int j = 0; j < attrCnt; j++)
     	{
-    		if(strcmp(attrName, attrList[j]) == 0)
+    		if(strcmp(attrName, attrList[j].attrName) == 0)
     		{
     			insertAttrList[i] = attrList[j];
     			insertAttrList[i].attrLen = attrs[i].attrLen;
-    			totalSizeinBytes += attrs[i].attrLen;
+    			record.length += attrs[i].attrLen;
     			break;
     		}
     	}
     }
 
-    
+    //Allocate mem for a record and copy it in void *data
+    record.data = malloc(record.length);
+    void *temp = record.data;
+    for(int i = 0; i < attrNum; i++)
+    {
+        temp = memcpy(temp, insertAttrList[i].attrValue, insertAttrList[i].attrLen);
+        temp = insertAttrList[i].attrLen + (char*)temp;
+    }
+
+    //Insert the record in the heapfile
+    Status returnStatus;
+    HeapFile heapfile(relation, returnStatus);
+    RID rid;
+    heapfile.insertRecord(record, rid);
+
+    //Delete memory
+    delete attrs;
+    delete insertAttrList;
+    free record.data;
 
     return OK;
 }
